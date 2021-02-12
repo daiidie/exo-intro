ee883f653a6736fc8c036a277f23b1d5af14903d
daiidie <delavegae46@gmail.com>
2021-02-12 12:20:45 +0100

    11022021

---
+++ pyramidgen.js
@@ -0,0 +1,10 @@
+let any = 'l' 
+
+console.log (`${any}              ${any}`)
+console.log (`${any}${any}            ${any}${any}`)
+console.log (`${any}${any}${any}          ${any}${any}${any}`)
+console.log (`${any}${any}${any}${any}        ${any}${any}${any}${any}`)
+console.log (`${any}${any}${any}${any}${any}      ${any}${any}${any}${any}${any}`)
+console.log (`${any}${any}${any}${any}${any}${any}    ${any}${any}${any}${any}${any}${any}`)
+console.log (`${any}${any}${any}${any}${any}${any}${any}  ${any}${any}${any}${any}${any}${any}${any}`)
+console.log (`${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}`)
